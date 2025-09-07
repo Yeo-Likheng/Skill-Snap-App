2d24743f8d9f7c1333f56645e3db4bb6e8ea5ee5
Likheng Yeo <2023342yeo@aupp.edu.kh>
2025-09-07 12:30:36 +0700

    set proxy

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,6 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT;
+app.set("trust proxy", 1);
 
 
 const uploadsDir = path.join(__dirname, "../uploads")
